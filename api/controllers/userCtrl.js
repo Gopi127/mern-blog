f809e21f6dad827c73bbbf970ecf05e04056b731
GOPI <mittaganupulagopi@gmail.com>
2024-02-10 11:56:13 +0530

    create a sign in page

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -2,6 +2,7 @@ const User = require("../model/User");
 const expressAsyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const userRegisterCtrl = expressAsyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -23,4 +24,35 @@ const userRegisterCtrl = expressAsyncHandler(async (req, res, next) => {
   }
 });
 
+const userLoginCtrl = expressAsyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(422, "Email or Password is required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc; // Exclude the password from the response
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = userRegisterCtrl;
+module.exports = userLoginCtrl;
--- api/router/userRouter.js
+++ api/router/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRegisterCtrl = require("../controllers/userCtrl");
+const userLoginCtrl = require("../controllers/userCtrl");
 
 const userRoutes = express.Router();
 
 userRoutes.post("/signup", userRegisterCtrl);
+userRoutes.post("/signin", userLoginCtrl);
 
 module.exports = userRoutes;
